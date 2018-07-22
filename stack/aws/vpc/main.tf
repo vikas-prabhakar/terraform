a6c21c2e7486c528f492fe936381cb4e7bc9413b
vikasdeepsharma <vikasdeep.sharma@paytm.com>
2018-07-22 16:54:37 +0530

    subnet input

--- stack/aws/vpc/main.tf
+++ stack/aws/vpc/main.tf
@@ -11,16 +11,22 @@ module "vpc" {
   source              = "git::git@github.com:vikas-prabhakar/terraform-vpc.git?ref=v0.0.1"
   name                = "stack"
   environment         = "prod"
-  vpc_cidr            = "10.0.0.0/24"
+  vpc_cidr            = "10.0.0.0/18"
   profile             = "default"
 }
 
-module "internet_gateway" {
-  source              = "git::git@github.com:vikas-prabhakar/terraform-internetgateway.git?ref=v0.0.1"
+module "external_subnet" {
+  source              = "git::git@github.com:vikas-prabhakar/terraform-publicsubnet.git?ref=v1.0.1"
   vpc_id              = "${module.vpc.vpc_id}"
   name                = "stack"
   environment         = "prod"
   profile             = "default"
+
+  public_subnets {
+     "10.0.1.0/24"   = "public"
+     "10.0.2.0/24" = "public"
+  }
+
 }
 
 output aws_vpc_id {
@@ -29,4 +35,12 @@ output aws_vpc_id {
 
 output aws_vpc_cidr {
   value = "${module.vpc.vpc_cidr}"
+}
+
+output aws_igw_id {
+  value = "${module.external_subnet.igw_id}"
+}
+
+output aws_public_id {
+  value = "${module.external_subnet.subnet_id}"
 }
